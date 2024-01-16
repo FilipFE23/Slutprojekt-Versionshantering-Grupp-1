beffe2ec22789e02b84f4096d3847591f907ce4b
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-16 16:14:25 +0100

    testing get/post me fetch

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 /*Tests for fetch*/
 const url =
-    "https://helloworld-9d8f2-default-rtdb.europe-west1.firebasedatabase.app/";
-fetch(url)
+    "https://spychat-a5f8e-default-rtdb.europe-west1.firebasedatabase.app/boys/.json";
+fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+        username: 1337,
+        password: "Fix my bugs",
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
     });
+
+//unfinsished function
+function userameAndPassword(name, password) { }
